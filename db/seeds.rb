19bba37e890ecc56d3d25a4cf2065da872025338
Great_Divide <bbirdwell+git@gmail.com>
2018-11-13 13:59:02 -0800

    update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-ben = User.create(username: "ben", email: "ben@gmail.com", password: "starlight123")
-brian = User.create(username: "brian", email: "brian@gmail.com", password: "sunshineabc")
+user1 = User.create(username: "user1", email: "user1@gmail.com", password: "starlight123")
+user2 = User.create(username: "user2", email: "user2@gmail.com", password: "sunshineabc")
 drill = Tool.create(name: "drill", description: "cordless dewalt")
-ben.tools << drill
+user1.tools << drill
